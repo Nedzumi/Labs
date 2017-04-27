d667cd4723ca48c68eb28b11b647e0d2a30475dc
ネズミ <y3jkp3@mail.ru>
2017-04-27 23:12:44 +0300

    Create somethingstrange.py

---
+++ OOP/lab2/somethingstrange.py
@@ -0,0 +1,31 @@
+import math
+
+class Number:
+    def __init__(self, s):
+        self.__s__ = s
+
+    def getNumber(self):
+        return self.__s__
+class Answer:
+    def __init__(self):
+        self.__x__ = 3
+
+    def getLA(self):
+        return self.__x__
+
+    def setLA(self, ans):
+        self.__x__ = ans
+class sqrt :
+    def setAns(self, x, s):
+        self.__ans__ = (x + (s / x)) /2
+
+    def getAns(self):
+        return self.__ans__
+
+if __name__ == '__main__':
+    print('Enter the number: ')
+    s = Number(float(input()))
+    x = Answer()
+    answer = sqrt ()
+    answer.setAns(x.getLA(), s.getNumber())
+    print('Answer = {}'.format(answer.getAns()))
