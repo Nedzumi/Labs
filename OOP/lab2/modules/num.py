d9361b84e1d6c1a066814b394da1c75e316f67ae
ネズミ <y3jkp3@mail.ru>
2017-04-27 22:34:03 +0300

    Delete num.py

--- OOP/lab2/modules/num.py
+++
@@ -1,7 +0,0 @@
-class Number:   # Класс, в котором сохраняется число, корень которого ищется
-    def __init__(self):
-        print('Enter the number in the square root: ')
-        self.__num__ = float(input())
-
-    def getNum(self):
-        return self.__num__
