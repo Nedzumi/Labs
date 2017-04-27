01c2b4e11e28c3badfed0f53f0d91d5e4e1b2a19
ネズミ <y3jkp3@mail.ru>
2017-04-27 10:20:10 +0400

    Create lab2.py

---
+++ OOP/applications/lab2.py
@@ -0,0 +1,25 @@
+eps = 0.001
+
+class Geron():
+    def __init__(self, x, y):
+        self.one=x
+        self.two=y
+    def formula(self):
+        count = 0
+        while abs(self.two - self.one) >= eps:
+            self.two = (self.two + self.one / self.two) / 2
+            count = count+1
+            if count == 10:
+                return self.one, self.two
+
+
+def main():
+    x = float(input('Число, для которого ищем корень: '))
+    y = int(input('Введите предположительное значение для корня: '))
+    grn=Geron(x,y)
+    grn.formula()
+    print (grn.formula())
+
+
+if __name__ == "__main__":
+main()
