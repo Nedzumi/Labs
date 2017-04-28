0200db37406d9bbcc4be5070f923276b5e0054d6
ネズミ <y3jkp3@mail.ru>
2017-04-28 14:12:01 +0300

    Delete output.py

--- OOP/lab4/modules/output.py
+++
@@ -1,8 +0,0 @@
-from matplotlib import pyplot as plt
-
-class MyOutput:
-    def graf(self, origData, answer, size):
-        fig, ax = plt.subplots()
-        ax.plot([i for i in range(30)], origData)
-        ax.plot([i for i in range(size, 30)], answer)
-        plt.show()
