abaea1ae4f57824433c80fac78322e50ff0b1f67
ネズミ <y3jkp3@mail.ru>
2017-04-28 14:12:48 +0300

    Update app.py

--- OOP/lab4/modules/app.py
+++ OOP/lab4/modules/app.py
@@ -1,15 +1,1 @@
-from modules.Modules import Core
 
-class App():
-    def __init__(self):
-        while 1:
-            self.Core = Core()
-            print('Введите X:')
-            self.Result = self.Core.Calculate(input())
-            print('Получаем скользящее среднее: ')
-            print(self.Result)
-            self.Core.BuildGraphic()
-            print('\nДля продолжения введите 1 \nДля выхода введите 0')
-            p = input()
-            if p == '0':
-                break;
