23fa7a055fbea17deb725c5ad65891e562d54742
ネズミ <y3jkp3@mail.ru>
2017-10-13 14:56:15 +0300

    Create custom.php

---
+++ kurs/css/custom.php
@@ -0,0 +1,72 @@
+body {background:url(/img/bg.png);height:100%;}
+h1,h2,h3 {color:#333333;text-shadow:2px 2px 0 #cccccc;font-weight:bold;}
+h1 {margin-bottom:20px;}
+
+header, #wrap {margin: 0 auto;width:960px;background:white;}
+#wrap {padding:10px 20px;min-height:100%;}
+header {border-left:5px solid black;border-right:5px solid black;}
+#pl-top {background:url(/img/plenka-top.png);height:43px;width:100%;}
+#pl-bottom {background:url(/img/plenka-bottom.png);height:43px;width:100%;}
+#pl-main {height:150px;}
+#pl-main .cadr {float:left;width:33%;border-left:5px solid black;}
+
+#logo {position:relative;}
+#logo img {height:100%;}
+#logo div {position:absolute;font-size:40px;color:#000000;font-weight:bold;text-shadow:3px 3px 0 #cccccc;}
+#logo #t1 {top:10px;left:130px;}
+#logo #t2 {top:55px;left:155px;}
+
+#genre {height:100%;padding:15px;}
+#genre select {height:55px;font-size:20px;}
+#genre button {width:100%;margin-bottom:5px;}
+
+#search {height:100%;padding:15px;}
+#search input {width:100%;height:55px;}
+#search button {width:100%;margin-bottom:5px;}
+
+.film {width:100%;margin-bottom:20px;}
+.film img {float:left;margin-right:20px;}
+.film h2 {margin-top:0;}
+.film p {margin-bottom:0px;}
+.film .orange {color:#f60;}
+.film a,.film a:hover {color:#333333;}
+.alternative {color:gray;font-size:14px;text-decoration:none;}
+
+.soon {text-align:center;}
+.soon a,.soon a:hover {color:#333333;}
+#main .hr {border-bottom:2px solid #f1f1f1;background:#333333;margin:10px;}
+
+#pagi {list-style:none;padding:0;}
+#pagi li {background:#000000;display:inline-block;margin:0 5px 5px 0;padding:5px;text-align:center;width:40px;}
+#pagi a {color:#ffffff;}
+#pagi a:hover {color:#f60;}
+
+table.table td:first-child {color:#f60}
+.gallery img {margin-right:5px;margin-bottom:5px;}
+.creator {float:left;width:100px;text-align:center;}
+div.heading {color:#f60;border-left:5px solid #f60;background:#f1f1f1;padding:10px;margin:10px 0;}
+ul#know {list-style:none;padding:0;}
+ul#know li {padding:10px 0 10px 45px;background-image:url(/img/ul-trivia.gif);background-repeat:no-repeat;background-position:15px 10px;border-bottom:1px solid #f1f1f1;}
+
+.slider.slider-horizontal {width:100%;}
+.slider-selection {background:lightblue;}
+#filmrandom {text-align:center;}
+
+#ninja {
+    background-color:#000000;
+    height:100%;
+    left:0;
+    opacity:0.5;
+    position:absolute;
+    top:0;
+    width:100%;
+    background-image:url(/img/loading.gif);
+    background-position:50%;
+    background-repeat:no-repeat;
+    background-size:300px;
+    display:none;
+}
+    
+    
+    
+    
