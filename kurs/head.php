273e3272d1199598ba5b1fd6dc800642ab477cd8
ネズミ <y3jkp3@mail.ru>
2017-10-13 14:53:44 +0300

    Create head.php

---
+++ kurs/head.php
@@ -0,0 +1,14 @@
+<?php 
+// шаблон <head> страницы, где мы подключаем нужные нам js-скрипты и стилевые файлы
+?>
+<head>
+    <meta content="text/html; charset=utf-8" http-equiv="content-type">
+    <script src="/js/jquery.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+    <script src="/js/bootstrap-slider.js"></script>
+    <script src="/js/custom.js"></script>
+    <link href="/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="/css/bootstrap-slider.css" rel="stylesheet" />
+    <link href="/css/custom.css" rel="stylesheet" />
+    
+</head>
